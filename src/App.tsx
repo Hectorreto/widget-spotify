68e094a5629be87cb76b88d219c639b6fb6da974
Héctor de Anda <30514545+Hectorreto@users.noreply.github.com>
2024-04-08 00:55:22 -0600

    refresh token and show currently playing image, song and artists

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
-import avatarImage from './assets/avatar.webp'
+import { useEffect, useState } from 'react'
+import { getCurrentlyPlaying } from './api/getCurrentlyPlaying'
+const token = 'BQBPyTjLbPKZtq6QNivK79294ebUQMuBvOoTxTCzq3dzRG41kBqBiHRwyUlm-aIPKkmyIsaHPZTjGGLVJ-_hWsvQS-sZFLNT5ut9Z-CyL_C0rs3p5eCOjWh-xDwK1g4s-koewk6Wn2_gbYj7dmAAYJ-DbmNKTjJtFnriZ5gQnq3oBEBiYxFIhUqhD79B3iHBeGA-9HYIMilwr6-pFl-AltaZ';
 
 function App() {
-  
+  const [artist, setArtist] = useState('');
+  const [song, setSong] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+
+  const updateData = async () => {
+    const data = await getCurrentlyPlaying(token)
+    const artists = data.item.artists;
+    if (Array.isArray(artists)) {
+      const names = artists.map((artist) => artist.name);
+      setArtist(names.join(', '));
+    }
+
+    setSong(data.item.name);
+    setImageUrl(data.item.album.images[1].url);
+  }
+
+  useEffect(() => {
+    const interval = setInterval(updateData, 10000);
+    return () => {
+      clearInterval(interval);
+    }
+  }, [])
 
   return (
     <main className='min-h-screen flex justify-center items-center'>
-      <div className="flex gap-8 bg-blue-950 bg-opacity-40 p-6 rounded-3xl text-pretty shadow-md">
+      <div className="flex gap-8 bg-blue-950 bg-opacity-40 p-6 rounded-3xl text-pretty shadow-md min-w-[800px]">
         <div className='w-[200px] h-[200px] rounded-[25px] shadow-2xl overflow-hidden'>
-          <img src={avatarImage} className="w-full h-full object-cover" alt="Song Image" />
+          <img src={imageUrl} className="w-full h-full object-cover" alt="Song Image" />
         </div>
         <div className="flex-1 flex flex-col">
-          <p className="text-gray-400 text-[35px]">Grimes</p>
-          <p className="text-gray-50 text-[35px]">So Heave I Fell Through the Earth </p>
+          <p className="text-gray-400 text-[35px]">{artist}</p>
+          <p className="text-gray-50 text-[35px]">{song}</p>
           <div className="flex-1 flex items-center">
             <div className="bg-gradient-to-r from-green-600 to-purple-600 rounded-full h-[8px] w-full"></div>
           </div>
---
+++ src/api/getRefreshToken.ts
@@ -0,0 +1,27 @@
+const refreshToken = 'AQB-y7JLBnMLoo9yAMTLXrwhXeScv-fpCWjkvJlOQq-3at1653i1pUGjQq7IblFNgE7CYM7AiGapIQUiYkKgNN6XiAoWcPtAcP7q2PtctN7D53pgkPDV59Z7mqqyUtZ_9bQ';
+const endpoint = "https://accounts.spotify.com/api/token";
+const clientId = import.meta.env.VITE_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
+
+export const getRefreshToken = async () => {
+  if (!clientId || !clientSecret) return;
+  try {
+    const authorization = btoa(`${clientId}:${clientSecret}`);
+    const response = await fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Basic ${authorization}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        grant_type: 'refresh_token',
+        refresh_token: refreshToken,
+        client_id: clientId
+      }),
+    })
+    const data = await response.json();
+    return data;
+  } catch(error) {
+    console.error(error);
+  }
+};
